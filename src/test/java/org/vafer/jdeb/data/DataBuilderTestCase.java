428fdd9e71d1c3437ecd693abe92a74b5c4cb057
Elliot West <teabot@gmail.com>
2011-08-08 14:26:00 +0100

    Data builder

--- src/test/java/org/vafer/jdeb/data/DataBuilderTestCase.java
+++ src/test/java/org/vafer/jdeb/data/DataBuilderTestCase.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.vafer.jdeb;
+package org.vafer.jdeb.data;
 
 import java.io.File;
 
@@ -22,33 +22,38 @@ import junit.framework.TestCase;
 
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.FileSet;
+import org.vafer.jdeb.Console;
+import org.vafer.jdeb.DataProducer;
+import org.vafer.jdeb.data.DataBuilder;
 import org.vafer.jdeb.producers.DataProducerFileSet;
 
-public class ProcessorTestCase extends TestCase {
+public class DataBuilderTestCase extends TestCase {
 
     /**
      * Checks if the file paths in the md5sums file use only unix file separators
      * (this test can only fail on Windows)
      */
     public void testBuildDataWithFileSet() throws Exception {
-        Processor processor = new Processor(new Console() {
+        Console console = new Console() {
             public void println(String s) {
             }
-        }, null);
-
+        };
+        
         Project project = new Project();
         project.setCoreLoader(getClass().getClassLoader());
         project.init();
 
         FileSet fileset = new FileSet();
-        fileset.setDir(new File(getClass().getResource("deb/data").toURI()));
+        fileset.setDir(new File(getClass().getResource("/org/vafer/jdeb/deb/data").toURI()));
         fileset.setIncludes("**/*");
         fileset.setProject(project);
 
-        StringBuilder md5s = new StringBuilder();
-        processor.buildData(new DataProducer[] { new DataProducerFileSet(fileset) }, new File("target/data.tar"), md5s, "tar");
+        DataBuilder dataBuilder = new DataBuilder(console);
+        dataBuilder.build(new DataProducer[] { new DataProducerFileSet(fileset) }, new File("target/data.tar"), "tar");
 
+        String md5s = dataBuilder.getMD5s();
         assertTrue("empty md5 file", md5s.length() > 0);
         assertFalse("windows path separator found", md5s.indexOf("\\") != -1);
     }
+    
 }
